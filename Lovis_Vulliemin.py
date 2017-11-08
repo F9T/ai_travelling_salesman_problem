c0181c9262547f0724d4cfe05c5672f0150ee576
kevin.vulliemi <kevin.vulliemin@he-arc.ch>
2017-11-08 14:13:32 +0100

    Populate with base files

---
+++ Lovis_Vulliemin.py
@@ -0,0 +1,12 @@
+
+def ga_solve(file=None, gui=True, maxtime=0):
+    distance = 0;
+
+    if(gui):
+        use_gui()
+
+    # Return total distance, for each files?
+    return distance
+
+def use_gui():
+    pass
